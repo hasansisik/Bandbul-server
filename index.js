b4c3cfc4c8301cdd833ff20b871151e5ef587b10
Hasan Şişik <sisik.hsn@gmail.com>
2025-09-15 21:44:22 +0300

    www

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'https://bandbul.vercel.app',
-        'https://bandbul-nextjs.vercel.app'
+        'https://bandbul-nextjs.vercel.app',
+        'https://bandbul.com',
+        'https://www.bandbul.com',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
